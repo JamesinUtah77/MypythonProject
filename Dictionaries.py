6826d043da01686e3c49a1ff61f4091b54506c07
JamesinUtah77 <88670087+JamesinUtah77@users.noreply.github.com>
2021-08-10 08:37:40 -0600

    Add files via upload

---
+++ Dictionaries.py
@@ -0,0 +1,20 @@
+
+
+# --------------------------------------------------
+def main():
+    """Make your noise here"""
+
+urls = {'Google':'http://google.com',
+        'Twitter':'http://twitter.com',
+        'WSU': 'http://weber.edu'}
+
+print(type(urls))
+print(urls)
+
+
+
+
+
+# --------------------------------------------------
+if __name__ == '__main__':
+    main()
